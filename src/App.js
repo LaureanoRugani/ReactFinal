4d5dfbbf0a6b29bcde20af65ae6e1b749bbff42c
laureanorugani <laureanorugani@gmail.com>
2022-08-15 14:26:39 -0300

    sincronizar counter y cartcontext

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
+import React from 'react';
 import '../src/components/NavBar/NavBar'
-import SaludoHome from './components/IntroHome/IntroHome';
+import IntroHome from './components/IntroHome/IntroHome';
 import NavBarLogo from './components/NavBar/NavBar';
 import ItemListContainer from './components/ItemListContainer/ItemListContainer'
 import 'bootswatch/dist/morph/bootstrap.min.css';
 import './Fonts/Fonts.css'
 import { Route, Routes } from 'react-router-dom';
 import ItemDetailContainer from './components/ItemDetailContainer/ItemDetailContainer';
+import CartProvider from './context/CartContext';
 
 
 function App() {
@@ -17,13 +19,15 @@ function App() {
             <div id='NavBar'>
                 <NavBarLogo/> 
             </div>
-            <SaludoHome/>
+            <IntroHome/>
             <br/>
+            <CartProvider>
             <Routes>
               <Route path='/' element={<ItemListContainer /> }/>
               <Route path='/categoria/:categoryId'  element={<ItemListContainer />}/>
               <Route path='/item/:id' element={<ItemDetailContainer />} />
             </Routes>
+            </CartProvider>
             <br/>
             <br></br>
           </div>
