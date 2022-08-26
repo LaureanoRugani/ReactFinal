f5f8fd67800e5fd55a4970021d0b790ae955d9c4
laureanorugani <laureanorugani@gmail.com>
2022-08-26 17:58:12 -0300

    arreglando firestore

--- src/Firebase/config.js
+++ src/Firebase/config.js
@@ -1,17 +1,40 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getFirestore } from "firebase/firestore";
 
-// Your web app's Firebase configuration
+
+
 const firebaseConfig = {
+
     apiKey: "AIzaSyCkjfCVbXs8TUo2SWKdlTWRKAK99m423vk",
+
     authDomain: "cursoreactlr.firebaseapp.com",
+
     projectId: "cursoreactlr",
+
     storageBucket: "cursoreactlr.appspot.com",
+
     messagingSenderId: "354983236698",
+
     appId: "1:354983236698:web:a151e4527401eaa80cf463",
+
     measurementId: "G-E8W9Z2R71K"
+
 };
 
-const app = initializeApp(firebaseConfig);
+
+
+export const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
+
+// Your web app's Firebase configuration
+// const firebaseConfig = {
+//     apiKey: "AIzaSyCkjfCVbXs8TUo2SWKdlTWRKAK99m423vk",
+//     authDomain: "cursoreactlr.firebaseapp.com",
+//     projectId: "cursoreactlr",
+//     storageBucket: "cursoreactlr.appspot.com",
+//     messagingSenderId: "354983236698",
+//     appId: "1:354983236698:web:a151e4527401eaa80cf463",
+//     measurementId: "G-E8W9Z2R71K"
+// };
+
+// const app = initializeApp(firebaseConfig);
--- src/components/ItemListContainer/ItemListContainer.js
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -2,48 +2,85 @@ import React from 'react'
 import { useState, useEffect } from "react";
 import ItemList from '../ItemList/ItemList'
 import { useParams } from 'react-router-dom';
-import { getFirestore, collection, getDocs, query, where } from "firebase/firestore";
+import { collection, getDocs, query, where } from "firebase/firestore";
+import { db } from '../../Firebase/config'; 
+
 
 export default function ItemListContainer() {
+
   const [loading, setLoading] = useState(true);
+
   const [error ,setError] = useState(false);
+
   const [resultado, setResultado] = useState([]);
+
   const { categoryId } = useParams ();
 
-
   useEffect(() => {
-    const queryDatabase = getFirestore ();
-    const queryCollection = collection(queryDatabase, 'viajes')
+
+    const getData = async() => {
+
     if(categoryId){
-      const queryFilter = query(queryCollection, where('viajes', '==', categoryId))
-      getDocs(queryFilter)
-        .then(res => setResultado(res.docs.map(producto => ({id: producto.id, ...producto.data()}))))
-        .catch(()=>{
-          setError(true)
-        })
-        .finally(()=> {
-          setLoading(false)
-        })
+
+      const q = query(collection(db, "viajes"), where("categoria", "==", categoryId));
+
+      const querySnapshot = await getDocs(q);
+
+      const data = querySnapshot.docs.map((doc) => {
+
+
+        return { ...doc.data(), id: doc.id}
+
+      });
+
+      setResultado(data);
+
+      setLoading(false)
+
     } else {
+
+      const queryCollection = collection(db, 'viajes')
+
       getDocs(queryCollection)
+
         .then(res => setResultado(res.docs.map(producto => ({id: producto.id, ...producto.data()}))))
+
         .catch(()=>{
+
           setError(true)
+
         })
+
         .finally(()=> {
+
           setLoading(false)
+
         })
-    }
-  
-  
+
+    }}
+
+    getData()
+
     }, [categoryId])
 
+
+
   return (
+
     <>
+
       {
+
         loading ? <h4 className='text-dark text-center bg-warning '>Cargando viajes...</h4> : <ItemList productos={resultado} />
+
       }
 
+
+
     </>
+
   )
+
 }
+
+
