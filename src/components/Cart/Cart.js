26bb8cc10bd91a287424de809160b0b691e4172d
laureanorugani <laureanorugani@gmail.com>
2022-09-05 02:05:58 -0300

    agregando card datos

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,63 +1,121 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import CartItem from "./CartItem/CartItem";
 import { useCartContext } from "../../context/CartContext";
-import './Cart.css'
 import { Link } from "react-router-dom";
+import './Cart.css'
 import Button from 'react-bootstrap/Button'
-import { addDoc, collection, getFirestore,doc,updateDoc } from "firebase/firestore";
+import { addDoc, collection, getFirestore, doc, updateDoc } from "firebase/firestore";
+import { Col, Row, Container } from "react-bootstrap";
+import Card from 'react-bootstrap/Card';
+import Form from 'react-bootstrap/Form';
+import Modal from 'react-bootstrap/Modal';
 
 const Cart = () => {
+    const [show, setShow] = useState(false);
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+
+    const [compra, setearCompra] = useState ([]);
+    useEffect(()=> {
+        const compra = JSON.parse(localStorage.getItem('compra'))
+        if(compra) {
+            setearCompra(compra)
+        }
+    }, [])
+
     const { cart, precioFinal, LimpiarCart } = useCartContext();
     const order = {
         buyer: {
-            name: 'Laureano',
-            email: 'lmr@gmail.com',
-            phone: '123123123',
-            address: '123'
+            name: 'lmr',
+            email: 'asd@gmail.com',
+            phone: '12345',
+            address: '54321111'
         },
         items: cart.map(producto => ({ id: producto.id, nombre: producto.nombre, precioUnitario: producto.precio, cantidad: producto.cantidad, subtotal: producto.precio * producto.cantidad })),
         total: precioFinal(),
     }
-
     const emitirCompra = () => {
         const dataBase = getFirestore();
         const ordersCollection = collection(dataBase, 'orders');
         addDoc(ordersCollection, order)
-            .then(({ id }) => alert(`felicitaciones, su cotizacion del viaje ha sido realizada con exito. Su numero de referencia es: ${id}`))
-        LimpiarCart()
-        cart.forEach((item)=> {
-            const itemRef = doc(dataBase, 'viajes', item.id)
+            .then(({ id }) =>         <Modal show={show} onHide={handleClose}>
+            <Modal.Header closeButton>
+            </Modal.Header>
+            <Modal.Body>(`felicitaciones, su pedido de cotizacion de viaje, fue realizado con exito. Su numero de referencia es: ${id}`)</Modal.Body>
+            <Modal.Footer>
+            <Button variant="primary" onClick={handleClose}>
+                Cerrar
+            </Button>
+            </Modal.Footer>
+        </Modal>  ) 
+        LimpiarCart() 
+        cart.forEach((item) => {
+            const itemRef = doc(dataBase, 'products', item.id)
             updateDoc(itemRef, {
-                stock: item.stock - item.cantidad ,
+                stock: item.stock - item.cantidad,
             })
         })
-    } 
+
+
+    }
 
     return (
         <>
             {
                 (cart.length === 0) ?
                     <div className="w-auto shadow-lg rounded d-flex row">
-                        <h3 className="text-dark text-center bg-warning">No hay viajes en tu carrito, por favor haga click en el boton debajo para volver al catalogo </h3>
+                        <h3 className="text-dark text-center bg-warning">No hay viajes en el carro de compras, vuelva al catalogo </h3>
                         <Link to={'/'}>
-                            <Button className="btn btn-info col-12">Volver al catálogo</Button>
+                            <Button className="btn btn-info col-12">Volver al catalogo de viajes</Button>
                         </Link>
                     </div>
                     :
-                    cart.map((producto) => {
-                        return (
-                            <CartItem key={producto.id} producto={producto} />
-                        )
+                    <Container fluid>
+                        <Row>
+                            <Col className="productCheckout">
+                                {
+                                    cart.map((producto) => {
+                                        return (
+                                            <Card className="cardProductCheckout mb-3">
+                                                <CartItem key={producto.id} producto={producto} />
+                                            </Card>
 
-                    })
+                                        )
+                                    })
+                                }
+                            </Col>
+                            <Col>
+                                <Card className="formUserCheckout  p-3">
+                                    <Form >
+                                        <Form.Group className="mb-3" controlId="formBasicEmail">
+                                            <Form.Label className="text-dark">Email</Form.Label>
+                                            <Form.Control type="email" placeholder="Ingrese su email" required />
+                                        </Form.Group>
+
+                                        <Form.Group className="mb-3" controlId="formBasicName">
+                                            <Form.Label className="text-dark">Nombre</Form.Label>
+                                            <Form.Control type="text" placeholder="Ingrese su nombre" required />
+                                        </Form.Group>
+                                        <Form.Group className="mb-3" controlId="formBasicAdress">
+                                            <Form.Label className="text-dark">Direccion</Form.Label>
+                                            <Form.Control type="text" placeholder="Ingrese su nombre direccion" />
+                                        </Form.Group>
+                                        <Form.Group className="mb-3" controlId="formBasicPhone">
+                                            <Form.Label >Telefono</Form.Label>
+                                            <Form.Control type="text" required placeholder="Ingrese su numero telefonico" />
+                                        </Form.Group>
+                                        <Button disabled={cart.length === 0} onClick={emitirCompra}>Checkout</Button>
+                                    </Form>
+                                </Card>
+                            </Col>
+                        </Row>
+                        <Card className="cartTotal">
+                            <h4 className="text-dark fw-bolder" >Total: ${precioFinal()}</h4>
+                        </Card>
+                    </Container>
 
             }
-            <div className="cartTotal">
-                <h4 >Total: ${precioFinal()}</h4>
 
-            </div>
-
-            <Button disabled={cart.length === 0} onClick={emitirCompra} className="btn btn-info col-12">Finalizar pedido</Button>
         </>
     )
 }
--- src/components/Cart/CartItem/CartItem.js
+++ src/components/Cart/CartItem/CartItem.js
@@ -6,9 +6,9 @@ import '../Cart.css'
 const CartItem = ({producto}) => {
     const {quitarProducto} = useCartContext();
     return (
-        <div className="itemCarrito w-auto shadow-lg rounded d-flex row  col-3">
-            <img src={producto.imagen} alt={producto.nombre} className="imgProductoCart img-fluid" />
-            <div className="itemCarritoBody d-flex row col p-1 mb-2">
+        <div className="w-auto shadow-lg rounded d-flex row col-2">
+            <img src={producto.imagen} alt={producto.nombre} />
+            <div className="d-flex row col p-3 mb-1">
                 <h4 >Nombre: {producto.nombre} </h4>
                 <p>Cantidad: {producto.cantidad} </p>
                 <p>Precio unitario: ${producto.precio}</p>
